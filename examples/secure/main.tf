f153231adf2f815f883dc219846a6f7fe3244d74
akocbek@ie.ibm.com <akocbek@ie.ibm.com>
2024-02-09 16:27:05 +0000

    add new secure example

---
+++ examples/secure/main.tf
@@ -0,0 +1,157 @@
+##############################################################################
+# Resource Group
+##############################################################################
+module "resource_group" {
+  source  = "terraform-ibm-modules/resource-group/ibm"
+  version = "1.1.4"
+  # if an existing resource group is not set (null) create a new one using prefix
+  resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
+  existing_resource_group_name = var.resource_group
+}
+
+
+#############################################################################
+# Provision cloud object storage and bucket
+#############################################################################
+
+resource "ibm_resource_instance" "cos_instance" {
+  count             = var.enable_vpc_flow_logs ? 1 : 0
+  name              = "${var.prefix}-vpc-logs-cos"
+  resource_group_id = module.resource_group.resource_group_id
+  service           = "cloud-object-storage"
+  plan              = var.cos_plan
+  location          = var.cos_location
+}
+
+resource "ibm_cos_bucket" "cos_bucket" {
+  count                = var.enable_vpc_flow_logs ? 1 : 0
+  bucket_name          = "${var.prefix}-vpc-logs-cos-bucket"
+  resource_instance_id = ibm_resource_instance.cos_instance[0].id
+  region_location      = var.region
+  storage_class        = "standard"
+}
+
+
+##############################################################################
+# Key Protect
+##############################################################################
+
+module "kp_all_inclusive" {
+  source                    = "terraform-ibm-modules/key-protect-all-inclusive/ibm"
+  version                   = "4.4.1"
+  key_protect_instance_name = "${var.prefix}-kp-instance"
+  resource_group_id         = module.resource_group.resource_group_id
+  region                    = var.region
+  resource_tags             = var.resource_tags
+  key_map = { "ocp" = [
+    "${var.prefix}-cluster-key",
+    "${var.prefix}-default-pool-boot-volume-encryption-key",
+    "${var.prefix}-other-pool-boot-volume-encryption-key"
+  ] }
+}
+
+##############################################################################
+# Base OCP Cluster in single zone
+##############################################################################
+locals {
+   cluster_vpc_subnets = {
+    default = [
+      for subnet in module.slz_vpc.subnet_zone_list :
+      {
+        id         = subnet.id
+        zone       = subnet.zone
+        cidr_block = subnet.cidr
+      }
+    ]
+  }
+
+  worker_pools = [
+    {
+      subnet_prefix     = "default"
+      pool_name         = "default" # ibm_container_vpc_cluster automatically names standard pool "default" (See https://github.com/IBM-Cloud/terraform-provider-ibm/issues/2849)
+      machine_type      = "bx2.4x16"
+      workers_per_zone  = 2
+      labels            = {}
+      resource_group_id = module.resource_group.resource_group_id
+      boot_volume_encryption_kms_config = {
+        crk             = module.kp_all_inclusive.keys["ocp.${var.prefix}-default-pool-boot-volume-encryption-key"].key_id
+        kms_instance_id = module.kp_all_inclusive.key_protect_guid
+      }
+    }
+  ]
+}
+
+module "ocp_base" {
+  source               = "terraform-ibm-modules/base-ocp-vpc/ibm"
+  version              = "3.10.2"
+  cluster_name         = var.prefix
+  ibmcloud_api_key     = var.ibmcloud_api_key
+  resource_group_id    = module.resource_group.resource_group_id
+  region               = var.region
+  force_delete_storage = true
+  vpc_id                          = module.slz_vpc.vpc_id
+  vpc_subnets          = local.cluster_vpc_subnets
+  worker_pools         = length(var.worker_pools) > 0 ? var.worker_pools : local.worker_pools
+  ocp_version          = var.ocp_version
+  tags                 = var.resource_tags
+  kms_config = {
+    instance_id = module.kp_all_inclusive.key_protect_guid
+    crk_id      = module.kp_all_inclusive.keys["ocp.${var.prefix}-cluster-key"].key_id
+  }
+  access_tags = var.access_tags
+}
+
+#############################################################################
+# Provision VPC
+#############################################################################
+
+module "slz_vpc" {
+  source                    = "terraform-ibm-modules/landing-zone-vpc/ibm"
+  version                   = "7.13.0"
+  resource_group_id                      = module.resource_group.resource_group_id
+  region                                 = var.region
+  name                                   = var.vpc_name
+  prefix                                 = var.prefix
+  tags                                   = var.resource_tags
+  access_tags                            = var.access_tags
+  enable_vpc_flow_logs                   = var.enable_vpc_flow_logs
+  create_authorization_policy_vpc_to_cos = var.create_authorization_policy_vpc_to_cos
+  existing_cos_instance_guid             = ibm_resource_instance.cos_instance[0].guid
+  existing_storage_bucket_name           = ibm_cos_bucket.cos_bucket[0].bucket_name
+  address_prefixes                       = var.address_prefixes
+  network_cidrs                          = var.network_cidrs
+  use_public_gateways = {
+    zone-1 = false
+    zone-2 = false
+    zone-3 = false
+  }
+}
+
+##############################################################################
+# SCC Workload Protection Instance
+##############################################################################
+
+module "scc_wp" {
+  source            = "terraform-ibm-modules/scc-workload-protection/ibm"
+  version           = "v1.1.0"
+  name              = "${var.prefix}-scc-wp"
+  region            = var.region
+  resource_group_id = module.resource_group.resource_group_id
+  resource_key_tags = var.resource_tags
+
+}
+
+##############################################################################
+# SCC Workload Protection Agent
+##############################################################################
+
+module "scc_wp_agent" {
+  source        = "../.."
+  cluster_name  = module.ocp_base.cluster_name
+  access_key    = module.scc_wp.access_key
+  region        = var.region
+  endpoint_type = "private"
+  name          = "${var.prefix}-scc-wp-agent"
+}
+
+##############################################################################
---
+++ examples/secure/variables.tf
@@ -0,0 +1,142 @@
+##############################################################################
+# Input Variables
+##############################################################################
+
+variable "ibmcloud_api_key" {
+  type        = string
+  description = "The IBM Cloud api token"
+  sensitive   = true
+}
+
+variable "prefix" {
+  type        = string
+  description = "Prefix for name of all resource created by this example"
+  default     = "pri-and-wp"
+  validation {
+    error_message = "Prefix must begin and end with a letter and contain only letters, numbers, and - characters."
+    condition     = can(regex("^([A-z]|[a-z][-a-z0-9]*[a-z0-9])$", var.prefix))
+  }
+}
+
+variable "region" {
+  type        = string
+  description = "Region where resources are created"
+  default     = "eu-gb"
+}
+
+variable "resource_group" {
+  type        = string
+  description = "An existing resource group name to use for this example, if unset a new resource group will be created"
+  default     = null
+}
+
+variable "resource_tags" {
+  type        = list(string)
+  description = "Optional list of tags to be added to created resources"
+  default     = []
+}
+
+variable "ocp_version" {
+  type        = string
+  description = "Version of the OCP cluster to provision"
+  default     = null
+}
+
+variable "worker_pools" {
+  type = list(object({
+    subnet_prefix     = string
+    pool_name         = string
+    machine_type      = string
+    workers_per_zone  = number
+    resource_group_id = optional(string)
+    labels            = optional(map(string))
+    boot_volume_encryption_kms_config = optional(object({
+      crk             = string
+      kms_instance_id = string
+      kms_account_id  = optional(string)
+    }))
+  }))
+  description = "List of worker pools."
+  default     = []
+}
+
+variable "access_tags" {
+  type        = list(string)
+  description = "Optional list of access management tags to be added to the created resources."
+  default     = []
+}
+
+##############################################################################
+
+
+variable "enable_vpc_flow_logs" {
+  type        = bool
+  description = "Enable VPC Flow Logs, it will create Flow logs collector if set to true"
+  default     = true
+}
+
+variable "cos_plan" {
+  description = "Plan to be used for creating cloud object storage instance"
+  type        = string
+  default     = "standard"
+  validation {
+    condition     = contains(["standard", "lite"], var.cos_plan)
+    error_message = "The specified cos_plan is not a valid selection!"
+  }
+}
+
+variable "cos_location" {
+  description = "Location of the cloud object storage instance"
+  type        = string
+  default     = "global"
+}
+
+variable "create_authorization_policy_vpc_to_cos" {
+  description = "Set it to true if authorization policy is required for VPC to access COS"
+  type        = bool
+  default     = true
+}
+
+variable "vpc_name" {
+  description = "The name of the vpc"
+  type        = string
+  default     = "vpc"
+}
+
+
+
+variable "address_prefixes" {
+  description = "OPTIONAL - IP range that will be defined for the VPC for a certain location. Use only with manual address prefixes"
+  type = object({
+    zone-1 = optional(list(string))
+    zone-2 = optional(list(string))
+    zone-3 = optional(list(string))
+  })
+  default = {
+    zone-1 = ["10.10.10.0/24"]
+    zone-2 = ["10.20.10.0/24"]
+    zone-3 = ["10.30.10.0/24"]
+  }
+  validation {
+    error_message = "Keys for `use_public_gateways` must be in the order `zone-1`, `zone-2`, `zone-3`."
+    condition     = var.address_prefixes == null ? true : (keys(var.address_prefixes)[0] == "zone-1" && keys(var.address_prefixes)[1] == "zone-2" && keys(var.address_prefixes)[2] == "zone-3")
+  }
+}
+
+variable "network_cidrs" {
+  description = "List of Network CIDRs for the VPC. This is used to manage network ACL rules for cluster provisioning."
+  type        = list(string)
+  default     = ["10.0.0.0/8", "164.0.0.0/8"]
+}
+
+
+variable "service_endpoints" {
+  description = "Service endpoints to use to create endpoint gateways. Can be `public`, or `private`."
+  type        = string
+  default     = "private"
+
+  validation {
+    error_message = "Service endpoints can only be `public` or `private`."
+    condition     = contains(["public", "private"], var.service_endpoints)
+  }
+}
