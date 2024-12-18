60b3fb876696bb57b139191c91738bfc38c6ec90
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-12-18 14:00:52 +0000

    chore(deps): update terraform-module

--- examples/secure/main.tf
+++ examples/secure/main.tf
@@ -15,7 +15,7 @@ module "resource_group" {
 
 module "kp_all_inclusive" {
   source                    = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                   = "4.18.1"
+  version                   = "4.18.2"
   key_protect_instance_name = "${var.prefix}-kp-instance"
   resource_group_id         = module.resource_group.resource_group_id
   region                    = var.region
@@ -72,7 +72,7 @@ locals {
 
 module "ocp_base" {
   source                       = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version                      = "3.35.7"
+  version                      = "3.35.8"
   cluster_name                 = var.prefix
   resource_group_id            = module.resource_group.resource_group_id
   region                       = var.region
