2df718e50c0b02e5f491060e3a7467e78912f166
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-12-28 08:40:10 +0000

    chore(deps): update terraform github.com/terraform-ibm-modules/terraform-ibm-landing-zone to v6.6.3

--- tests/resources/existing-resources/standard/main.tf
+++ tests/resources/existing-resources/standard/main.tf
@@ -15,7 +15,7 @@ module "resource_group" {
 ##############################################################################
 
 module "landing_zone" {
-  source                 = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks//module?ref=v6.6.2"
+  source                 = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks//module?ref=v6.6.3"
   region                 = var.region
   prefix                 = var.prefix
   tags                   = var.resource_tags
