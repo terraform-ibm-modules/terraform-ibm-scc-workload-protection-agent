c687c06a666695a18595021525e94c6c3b8c4382
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-04-05 17:09:49 +0000

    chore(deps): update terraform-module

--- tests/resources/existing-resources/standard/main.tf
+++ tests/resources/existing-resources/standard/main.tf
@@ -4,7 +4,7 @@
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.1.6"
+  version = "1.2.0"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -15,7 +15,7 @@ module "resource_group" {
 ##############################################################################
 
 module "landing_zone" {
-  source                 = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks//module?ref=v7.3.1"
+  source                 = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks//module?ref=v7.4.3"
   region                 = var.region
   prefix                 = var.prefix
   tags                   = var.resource_tags
@@ -32,7 +32,7 @@ module "landing_zone" {
 
 module "scc_wp_instance" {
   source            = "terraform-ibm-modules/scc-workload-protection/ibm"
-  version           = "1.5.2"
+  version           = "1.5.6"
   name              = "${var.prefix}-scc-wp-instance"
   region            = var.region
   resource_group_id = module.resource_group.resource_group_id
