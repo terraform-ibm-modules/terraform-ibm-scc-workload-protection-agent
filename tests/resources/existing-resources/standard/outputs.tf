879643864ad2f34ebb02f85553db23c4c6233eba
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2025-04-18 15:31:31 +0100

    fix: updated catalog validation (#294)

--- tests/resources/existing-resources/standard/outputs.tf
+++ tests/resources/existing-resources/standard/outputs.tf
@@ -4,22 +4,12 @@ output "access_key" {
   sensitive   = true
 }
 
-output "cluster_data" {
-  value       = module.landing_zone.cluster_data
-  description = "Details of OCP cluster."
+output "cluster_id" {
+  value       = module.ocp_base.cluster_id
+  description = "ID of the cluster."
 }
 
-output "workload_cluster_id" {
-  value       = module.landing_zone.workload_cluster_id
-  description = "ID of the workload cluster."
-}
-
-output "workload_cluster_name" {
-  value       = module.landing_zone.workload_cluster_name
-  description = "Name of the workload cluster."
-}
-
-output "cluster_resource_group_id" {
-  value       = module.landing_zone.cluster_data["${var.prefix}-workload-cluster"].resource_group_id
-  description = "Resource group ID of the workload cluster."
+output "resource_group_id" {
+  value       = module.resource_group.resource_group_id
+  description = "Resource group ID"
 }
