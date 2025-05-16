674a481915105f416847fb9d468a784267ff9bfb
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-05-16 11:48:29 +0000

    fix(deps): update helm release sysdig-deploy to v1.83.1

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "helm_release" "scc_wp_agent" {
   name             = var.name
   repository       = "https://charts.sysdig.com"
   chart            = "sysdig-deploy"
-  version          = "1.82.1"
+  version          = "1.83.1"
   namespace        = var.namespace
   create_namespace = true
   timeout          = 1500
