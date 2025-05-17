de2df37e07940cab8be40b515a62e3f051783e0c
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-05-17 11:33:50 +0000

    fix(deps): update terraform dependencies

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -71,7 +71,7 @@ locals {
 module "ocp_base" {
   count                = var.is_openshift && var.is_vpc_cluster ? 1 : 0
   source               = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version              = "3.46.19"
+  version              = "3.47.1"
   cluster_name         = var.prefix
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
