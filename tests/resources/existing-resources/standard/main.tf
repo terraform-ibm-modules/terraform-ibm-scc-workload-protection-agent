04b3e17bff9b4f3fc67b6e7513784184942fea5a
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-05-25 01:46:10 +0000

    fix(deps): update terraform dependencies

--- tests/resources/existing-resources/standard/main.tf
+++ tests/resources/existing-resources/standard/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "scc_wp_instance" {
   source            = "terraform-ibm-modules/scc-workload-protection/ibm"
-  version           = "1.5.12"
+  version           = "1.6.0"
   name              = "${var.prefix}-scc-wp-instance"
   region            = var.region
   resource_group_id = module.resource_group.resource_group_id
@@ -78,7 +78,7 @@ locals {
 
 module "ocp_base" {
   source               = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version              = "3.48.0"
+  version              = "3.48.2"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   tags                 = var.resource_tags
