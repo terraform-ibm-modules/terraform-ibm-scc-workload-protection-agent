ada5852abf3e87c8275ec0765551443ed5988db4
Kierra Searle <kierra@searle.garden>
2025-07-15 04:55:16 -0400

    chore: bump terraform help version to v3 and fix errors (#368)
    * chore: bump terraform help version to v3 and fix errors
    
    * fix: helm required provider limit

--- examples/basic/version.tf
+++ examples/basic/version.tf
@@ -7,7 +7,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.8.0, < 3.0.0"
+      version = ">= 3.0.0, <4.0.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
